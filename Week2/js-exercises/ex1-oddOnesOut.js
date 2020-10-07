d1284cdc0ba9ce47cc8b99c76d7a2864104fab85
Vaida <93vaida@gmail.com>
2020-10-07 14:07:46 +0200

    HOF that filters even numbers and doubles them

--- Week2/js-exercises/ex1-oddOnesOut.js
+++ Week2/js-exercises/ex1-oddOnesOut.js
@@ -6,7 +6,6 @@
  Avoid using for loop or forEach.
  The function should still behave the same.
 
- */
 function doubleEvenNumbers(numbers) {
   const newNumbers = [];
   for (let i = 0; i < numbers.length; i++) {
@@ -16,6 +15,17 @@ function doubleEvenNumbers(numbers) {
   }
   return newNumbers;
 }
-
+ */
 const myNumbers = [1, 2, 3, 4];
-console.log(doubleEvenNumbers(myNumbers)); // Logs "[4, 8]" to the console
+//using filter to get only even numbers:
+const evenNumbers = myNumbers.filter((number) => number % 2 === 0);
+console.log(evenNumbers);
+//using map to double all the numbers
+const doubleEvenNumbers = evenNumbers.map((number) => number * 2);
+console.log(doubleEvenNumbers);
+
+//we could also combine filter and map together: (NOT SURE IF THIS WOULD BE APPROPRIATE WAY TO WRITE HIGHER ORDER FUNCTION???)
+const evenNumbersDoubled = myNumbers
+  .filter((number) => number % 2 === 0)
+  .map((number) => number * 2);
+console.log(evenNumbersDoubled);
