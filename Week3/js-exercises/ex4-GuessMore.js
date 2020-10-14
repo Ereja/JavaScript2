f9310233471c4f139e230742ec952bd7d6c5b1a9
Vaida <93vaida@gmail.com>
2020-10-14 16:19:36 +0200

    explanasion of passing by value vs passing by reference

--- Week3/js-exercises/ex4-GuessMore.js
+++ Week3/js-exercises/ex4-GuessMore.js
@@ -16,10 +16,14 @@ function f1(val) {
 }
 f1(x);
 console.log(x);
+/* Console log will display 9 as this is value of x.
+Outcome of f1(x) is 10 but this is not what we are logging into console
+*/
 
 const y = {
   x: 9
 };
+//declaring an object y, which contains property x with value of 9
 
 function f2(val) {
   val.x = val.x + 1;
@@ -27,3 +31,9 @@ function f2(val) {
 }
 f2(y);
 console.log(y);
+
+/*
+f2(y) will change value of x to 10
+Object y value is changed from 9 to 10 because arrays and objects are passed by refference. We are passing object as an argument to the function and it changes objects value globally.
+
+*/
