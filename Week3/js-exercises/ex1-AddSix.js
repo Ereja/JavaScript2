c1621fb5af9a8c40a637f8b28e06e8c0541c3132
Vaida <93vaida@gmail.com>
2020-10-14 17:28:46 +0200

    function that returns number incremented by six

--- Week3/js-exercises/ex1-AddSix.js
+++ Week3/js-exercises/ex1-AddSix.js
@@ -6,15 +6,21 @@ Declare a function called `createBase`.The function takes a number as a paramete
 return a closure, that adds a number to the base number argument.
 
 Call the function three times. The return values should be:
- 15, 24,  36
+ 15, 24,  33
 
  */
 
-function createBase( /* ???? */ ) {
-  // Put here your logic...
+function createBase(number) {
+  return function() {
+    return number += 9
+  }
 }
 
 const addSix = createBase(6);
 
 // Put here your function calls...
 console.log(addSix());
+console.log(addSix());
+console.log(addSix());
+
+
